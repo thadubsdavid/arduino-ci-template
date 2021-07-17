db1971c8cf7371cdddafdff5e4177941f4d0d7ef
thadubsdavid <davidpatrickadam@gmail.com>
2021-07-17 22:52:35 +0200

    added build yml and compiler script

---
+++ ci/build-arduino.sh
@@ -0,0 +1,36 @@
+# This script is going to install the Arduino CLI on our virtual machine and compile our code for several Arduino boards.
+
+#!/bin/bash
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Enable the globstar shell option
+shopt -s globstar
+
+# Make sure we are inside the github workspace
+cd $GITHUB_WORKSPACE
+
+# Create directories
+mkdir $HOME/Arduino
+mkdir $HOME/Arduino/libraries
+
+# Install Arduino IDE
+export PATH=$PATH:$GITHUB_WORKSPACE/bin
+curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
+arduino-cli config init
+arduino-cli core update-index
+
+# Install Arduino AVR core
+arduino-cli core install arduino:avr
+arduino-cli core install arduino:samd
+
+
+# Link Arduino library
+ln -s $GITHUB_WORKSPACE $HOME/Arduino/libraries/CI_Test_Library
+
+# Compile all *.ino files for the Arduino Uno
+for f in **/*.ino ; do
+    arduino-cli compile -b arduino:avr:uno $f
+    arduino-cli compile -b arduino:samd:arduino_zero_native $f
+done
+
