20cbbb6d77243aea4bf3cf933b5ecfb53f9ea248
thadubsdavid <davidpatrickadam@gmail.com>
2021-07-17 22:36:42 +0200

    added some digitalWriting and logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
+#define onboard 13
+
+
 
 void setup() {
+  pinMode(onboard, OUTPUT);
+  Serial.begin(9600);
   // put your setup code here, to run once:
 }
 
 void loop() {
+  digitalWrite(onboard, LOW);
+  delay(1000);
+  digitalWrite(onboard, HIGH);
+  delay(1000);
+  Serial.println("some logging");
   // put your main code here, to run repeatedly:
 }
